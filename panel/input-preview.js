747bbd2709bce76332fcba360408556ab0632324
Johnny Wu <KarasAya@gmail.com>
2016-05-13 21:17:20 +0800

    remove unused deps

---
+++ panel/input-preview.js
@@ -0,0 +1,45 @@
+(() => {
+  'use strict';
+
+  let timeoutID = null;
+
+  function _updateEventText ( viewEL, name ) {
+    let eventEL = viewEL.querySelector(`.event`);
+    clearTimeout(timeoutID);
+    eventEL.innerHTML = `event: ${name}`;
+    timeoutID = setTimeout(() => {
+      eventEL.innerHTML = 'event: none';
+    }, 200);
+  }
+
+  return function init ( viewEL ) {
+    Editor.import('packages://ui-kit-preview/panel/input-preview.tmpl').then(
+      content => {
+        viewEL.innerHTML = content;
+
+        // g-01
+        ['.g-01', '.g-02'].forEach(g => {
+          let input = viewEL.querySelector(`${g} ui-input`);
+
+          let text = viewEL.querySelector(`${g} span.text`);
+          text.innerHTML = input.value;
+
+          input.addEventListener('input', () => {
+            let text = viewEL.querySelector(`${g} span.text`);
+            text.innerHTML = input.value;
+
+            _updateEventText(viewEL, 'input');
+          });
+
+          input.addEventListener('confirm', () => {
+            _updateEventText(viewEL, 'confirm');
+          });
+
+          input.addEventListener('cancel', () => {
+            _updateEventText(viewEL, 'cancel');
+          });
+        });
+      }
+    );
+  };
+})();
--- panel/panel.js
+++ panel/panel.js
@@ -23,8 +23,12 @@ Editor.Panel.extend({
     }
 
     div.section {
-      border-bottom: 1px solid #333;
+      border-bottom: 1px solid #666;
       padding-bottom: 10px;
+    }
+
+    div.section:last-child {
+      border-bottom: 0px;
     }
 
     div.group {
@@ -39,7 +43,9 @@ Editor.Panel.extend({
   template: `
     <div class="toolbar">
       <select id="select">
-        <option value="ui-button">ui-button</option>
+        <option value="button">ui-button</option>
+        <option value="checkbox">ui-checkbox</option>
+        <option value="input">ui-input</option>
       </select>
     </div>
     <div id="view"></div>
@@ -54,10 +60,33 @@ Editor.Panel.extend({
       view: this.shadowRoot.querySelector('#view'),
     };
 
-    Editor.import('packages://ui-kit-preview/panel/button-preview.js').then(
-      initFn => {
+    this.$.select.addEventListener('change', event => {
+      let value = event.target.value;
+
+      this.profiles.local.scrollTop = 0;
+      this.profiles.local.select = value;
+      this.profiles.local.save();
+
+      this.showPreview(value);
+    });
+
+    this.$.select.value = this.profiles.local.select;
+    this.showPreview(this.profiles.local.select);
+  },
+
+  close () {
+    this.profiles.local.scrollTop = this.$.view.scrollTop;
+    this.profiles.local.save();
+  },
+
+  showPreview (name) {
+    Editor
+      .import(`packages://ui-kit-preview/panel/${name}-preview.js`)
+      .then(initFn => {
         initFn(this.$.view);
-      }
-    );
+        setTimeout(() => {
+          this.$.view.scrollTop = this.profiles.local.scrollTop;
+        }, 10);
+      });
   },
 });
